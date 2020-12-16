007e20c375c0ed6a3e1303740f94655b8b5acd48
Elisha Hung <mrhchief@gmail.com>
2020-12-16 17:50:09 +0800

    add version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="moonshine",
-    version="0.0.2",
+    version="0.0.3",
     author="eli Hung",
     author_email="mrhchief@gmail.com",
     description="toolkit for moonshine node-server",
