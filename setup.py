c68b2ae7ce2f13ba866aef0af952850d020d6f36
Elisha Hung <mrhchief@gmail.com>
2020-12-16 12:54:16 +0800

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="moonshine-ELI",
+    version="0.0.1",
+    author="eli Hung",
+    author_email="mrhchief@gmail.com",
+    description="toolkit for moonshine node-server",
+    long_description="toolkit for moonshine node-server",
+    long_description_content_type="text",
+    url="https://github.com/MoonShineVFX/node-package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
